7f5ff119b73a494476948e3b81bc52cb8d0aadfa
raj-diwate <rajdiwate23@gmail.com>
2026-03-15 18:20:18 +0530

    Update create_indexes.sql

--- schema/create_indexes.sql
+++ schema/create_indexes.sql
@@ -1,11 +1,14 @@
-CREATE INDEX idx_query_id
-ON query_logs(query_id);
+-- Index on query_logs for faster filtering
+CREATE INDEX idx_query_logs_query_id ON query_logs(query_id);
+CREATE INDEX idx_query_logs_user_id ON query_logs(user_id);
+CREATE INDEX idx_query_logs_database_id ON query_logs(database_id);
+CREATE INDEX idx_query_logs_execution_time ON query_logs(execution_time_ms);
 
-CREATE INDEX idx_user_id
-ON query_logs(user_id);
+-- Index on frequent_queries
+CREATE INDEX idx_frequent_queries_execution_count ON frequent_queries(execution_count);
 
-CREATE INDEX idx_database_id
-ON query_logs(database_id);
+-- Index on inefficient_queries
+CREATE INDEX idx_inefficient_queries_ratio ON inefficient_queries(inefficiency_ratio);
 
-CREATE INDEX idx_execution_time
-ON query_logs(execution_time_ms);
+-- Index on slow_queries
+CREATE INDEX idx_slow_queries_avg_time ON slow_queries(avg_execution_time);
